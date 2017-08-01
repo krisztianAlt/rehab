4a67afb3a4b473a868e4a895226bb181cf9d3e71
krisztian <alt.krisztian@gmail.com>
2017-08-01 20:46:05 +0200

    Test touch

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def windmill_game():
     return render_template('windmill.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
