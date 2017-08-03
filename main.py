a0373fb943d9e52f4e6dba19098d734dd12f3fae
krisztian <alt.krisztian@gmail.com>
2017-08-03 07:21:10 +0200

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def windmill_game():
-    return render_template('windmill.html')
+    return render_template('windmill_resize_test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/responsive_images.js
+++ static/responsive_images.js
@@ -1,5 +1,6 @@
 function set_body_height() { // set body height = window height
     $('body').height($(window).height());
+    console.log('I am in resp-HEIGHT section.');
 }
 
 $(document).ready(function() {
